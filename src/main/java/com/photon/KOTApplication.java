d0d591e144d910d0bec2e727d40da38668265905
Sathish Thangadurai <sathish.thangadurai@photon.com>
2024-07-17 13:22:37 +0530

    initial commit

---
+++ src/main/java/com/photon/KOTApplication.java
@@ -0,0 +1,12 @@
+package com.photon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KOTApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(KOTApplication.class, args);
+    }
+}
